c6047fc794d3f992b9e98fa3e8565c006b894b72
otto-dev <ignatius.public@gmail.com>
2021-11-09 19:09:15 +1100

    Update benchmark

--- benchmarks/src/main/scala/cats/effect/benchmarks/FairnessBenchmark.scala
+++ benchmarks/src/main/scala/cats/effect/benchmarks/FairnessBenchmark.scala
@@ -16,10 +16,11 @@
 
 package cats.effect.benchmarks
 
-import cats.effect.IO
+import cats.effect.{FiberIO, IO}
 import cats.effect.std.Queue
 import cats.implicits._
 import cats.effect.implicits._
+import org.openjdk.jmh.infra.Blackhole
 import org.openjdk.jmh.annotations.{
   Benchmark,
   BenchmarkMode,
@@ -36,60 +37,56 @@ import scala.util.hashing.MurmurHash3
 @State(Scope.Thread)
 @BenchmarkMode(Array(Mode.Throughput))
 @OutputTimeUnit(TimeUnit.MINUTES)
-class ComputationHeavyBenchmark {
-  @Param(Array("1000"))
+class FairnessBenchmark {
+  @Param(Array("100"))
   var size: Int = _
 
-  val outputCount = 100
-  val computeIterations = 1000
+  val parallelism = 10
+  val computations = 10000
 
   @Benchmark
-  def scheduling(): Unit = {
+  def scheduling(blackhole: Blackhole): Unit = {
     import cats.effect.unsafe.implicits.global
     val io = for {
-      inputs <- Queue.unbounded[IO, Int]
-      outputs <- Queue.unbounded[IO, Seq[Int]]
+      request <- Queue.unbounded[IO, Int]
+      response <- Queue.unbounded[IO, Seq[Int]]
+      unfairness <- createUnfairness
       source <- (0 until size)
-        .map(i => IO.blocking(() /* simulate external source */ ) >> inputs.offer(i))
+        .map(i => IO.blocking(() /* simulate external source */ ) >> request.offer(i))
         .toList
         .sequence
         .start // data source
-      _ <- outputs
+      _ <- response
         .take
-        .map(_ => IO.blocking(() /* simulate output consumer */ ))
+        .map(v => IO.blocking(blackhole.consume(v) /* simulate output consumer */ ))
         .void
         .foreverM
         .start // output drain
-      _ <- computeElementOutput(inputs, outputs)(1)
+      _ <- processNextRequest(request, response)(1)
       _ <- source.join
+      _ <- unfairness.traverse(_.cancel)
     } yield ()
     io.unsafeRunSync()
   }
 
-  def computeElementOutput(inputs: Queue[IO, Int], outputs: Queue[IO, Seq[Int]])(
+  def createUnfairness: IO[List[FiberIO[Nothing]]] =
+    (0 until (Runtime.getRuntime().availableProcessors() * 2))
+      .toList
+      .traverse(x => murmurMe(0)(x).foreverM.start)
+
+  def processNextRequest(inputs: Queue[IO, Int], outputs: Queue[IO, Seq[Int]])(
       iteration: Int): IO[Unit] = for {
     element <- inputs.take
-    result <- (0 until outputCount).map(neuralNetworkInput(element)).toList.parSequence
+    result <- (0 until parallelism).toList.parTraverse(_ => murmurMe(0)(element))
     _ <- outputs.offer(result)
     _ <-
-      if (iteration < size) IO.cede >> computeElementOutput(inputs, outputs)(iteration + 1)
+      if (iteration < size)
+        IO.cede >> processNextRequest(inputs, outputs)(iteration + 1)
       else IO.unit
   } yield ()
 
-  // Simulate calculating a computation-heavy neural network feature (input) based on each
-  // element received from the queue
-  private def neuralNetworkInput(element: Int)(seed: Int) = for {
-    _ <- computeSomething(element, seed)(0)
-    _ <- IO.cede
-  } yield element
-
-  private def computeSomething(a: Int, b: Int)(i: Int): IO[Int] = {
-    if (i == computeIterations) IO.pure(a)
-    else
-      for {
-        a2 <- IO(MurmurHash3.arrayHash(Array[Int](a, b), b))
-        _ <- IO.cede
-        result <- computeSomething(a2, b)(i + 1)
-      } yield result
+  private def murmurMe(i: Int)(x: Int): IO[Int] = {
+    if (i == computations) IO.pure(x)
+    else IO.cede >> IO.defer(murmurMe(i + 1)(MurmurHash3.stringHash(s"$x,$i")))
   }
 }
